e6219cf736cb0704c4566ea485ea7f6a76b5e32b
Andrew Huth <git@huth.me>
2024-10-01 19:37:35 -0400

    Flap those wings

--- src/components/Bird.tsx
+++ src/components/Bird.tsx
@@ -1,5 +1,13 @@
+import clsx from 'clsx';
+import styles from './Bird.module.css';
+
 export default function Bird() {
   return (
-    <div className="h-8 w-11 bg-[url('/bluebird-midflap.png')] bg-cover" />
+    <div
+      className={clsx(
+        "h-8 w-11 bg-[url('/bluebird-midflap.png')] bg-cover",
+        styles.bird,
+      )}
+    />
   );
 }
